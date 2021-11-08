739a28dc5c7e0983cb275e95b56e0965f16aa21c
Luecha Kanmaneekul <luecha006@gmail.com>
2021-11-08 12:32:53 +0700

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'workshop-primeng12';
+
+  countries!: any[];
+
+  selectedCity1: any;
+  value1: any;
+
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import {AccordionModule} from 'primeng/accordion';
+import {CascadeSelectModule} from 'primeng/cascadeselect';
+import { InputTextModule } from "primeng/inputtext";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    CascadeSelectModule,
+    FormsModule,
+    InputTextModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
